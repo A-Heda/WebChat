3c47ba630d452b53c10e183b35830f8cf1cb76de
AmirAli <saah1386@gmail.com>
2026-06-23 20:16:38 +0330

    Changed login.js so it recives Username not UserId..

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,13 +1,13 @@
-function login(){
+async function login(){
 
-    const id =
+    const username =
         document.getElementById("id").value;
 
     const password =
         document.getElementById("password").value;
 
     if(id === ""){
-        alert("User ID is required.");
+        alert("Username is required.");
         return;
     }
 
@@ -16,5 +16,44 @@ function login(){
         return;
     }
 
-    alert("Welcome to Blink!");
+    try {
+
+        const response = await fetch(
+            "http://localhost:8080/users/login",
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            }
+        );
+
+
+        const data = await response.json();
+
+
+        if (response.ok) {
+
+            alert("Login successful!");
+
+            localStorage.setItem("username",username);
+
+            window.location.href ="../chat/chat.html";
+
+        } else {
+            alert(data);
+        }
+
+    } catch (error) {
+
+        alert("Cannot connect to server.");
+
+        console.error(error);
+    }
 }
